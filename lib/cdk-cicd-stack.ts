3b7a12575f559443d1f02cddf7c50ec2512e8f60
DacDinh147 <mmlgd46@gmail.com>
2024-07-13 14:29:50 +0700

    update

--- lib/cdk-cicd-stack.ts
+++ lib/cdk-cicd-stack.ts
@@ -1,9 +1,27 @@
 import * as cdk from 'aws-cdk-lib';
+import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 import { Construct } from 'constructs';
+import { SecretValue } from 'aws-cdk-lib';
 
 export class CdkCicdStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    const githubToken = SecretValue.secretsManager('github-token');
+
+    new CodePipeline(this, 'AwesomePipeline', {
+      pipelineName: 'AwesomePipeline',
+      synth: new ShellStep('Synth', {
+        input: CodePipelineSource.gitHub('DacDInh147/cdk-cicd', 'main', {
+          authentication: githubToken,
+        }),
+        commands: [
+          'npm ci',
+          'npx cdk synth'
+        ],
+        primaryOutputDirectory: 'cdk.out'
+      })
+    })
 
   }
 }
